26f905dd45b2518679bd4ce5c65046f367694ef5
Md. Tausif Hossain <tausif.iub@gmail.com>
2023-05-28 10:59:46 +0600

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Home(){
   return(
     <div>
+      <nav>
+        <Link to='/about'>About Us</Link>
+        <Link to='/contact'>Contact Us</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { About} from './App';
+import App, { Contact } from './App';
+import { About } from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path='/' element={<App></App>}></Route>
-    <Route path='/about' element={<About></About>}></Route>
+    <Routes>
+      <Route path='/' element={<App></App>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/contact' element={<Contact></Contact>}></Route>
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
